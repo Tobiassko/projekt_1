6cbe32897bb77230aaf436fffe6b9657d5eb849a
Tobias, Olofsson <tobias.olofsson@edu.sgy.se>
2024-09-25 21:47:10 +0200

    fixed block_box

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ def block_color():
     current_color_index +=1
     current_blocks = getattr(bl, block_catagorys[current_color_index-1])
     if current_color_index == 16:
-        current_color_index = 0        
+        current_color_index = 0
+    if 'block_box' in globals():
+        block_box['values'] = list(current_blocks.keys())
+        block_box.set('Select a block')
 
 
 
@@ -46,12 +49,12 @@ def random_block():
     # Check if the diamond switch is off
     if use_diamonds.get() == False:
         # Set the random selection string with the block details
-        rand_selection.set(f"Block: {ran_block}\nValue: {value}\nStack: {value*64:,}\nChest: {value*1728:,}\nDouble Chest: {value*3456:,}\nChunk: {value*98304:,}")
+        rand_selection.set(f"Block: {ran_block}\nValue: {value}\nStack: {value*64:,.0f}\nChest: {value*1728:,.0f}\nDouble Chest: {value*3456:,.0f}\nChunk: {value*98304:,.0f}")
     else:
         # If the diamond switch is on, adjust the value accordingly
         value = value / 1500
         # Set the random selection string with the adjusted block details
-        rand_selection.set(f"Block: {ran_block}\nValue: {value}\nStack: {value*64:,}\nChest: {value*1728:,}\nDouble Chest: {value*3456:,}\nChunk: {value*98304:,}")
+        rand_selection.set(f"Block: {ran_block}\nValue: {value}\nStack: {value*64:,.0f}\nChest: {value*1728:,.0f}\nDouble Chest: {value*3456:,.0f}\nChunk: {value*98304:,.0f}")
     
     # Print the selected block and its value for debugging purposes
     print(ran_block)
@@ -66,18 +69,21 @@ def diamond_switch():
     value = (current_blocks[ran_block])
     if use_diamonds.get() == True:
         value = round(value/1500, 2)
-        rand_selection.set(f"Block: {ran_block}\nValue: {value}\nStack: {value*64:,}\nChest: {value*1728:,}\nDouble Chest: {value*3456:,}\nChunk: {value*98304:,}")
+        rand_selection.set(f"Block: {ran_block}\nValue: {value}\nStack: {value*64:,.0f}\nChest: {value*1728:,.0f}\nDouble Chest: {value*3456:,.0f}\nChunk: {value*98304:,.0f}")
     else:
-        rand_selection.set(f"Block: {ran_block}\nValue: {value}\nStack: {value*64:,}\nChest: {value*1728:,}\nDouble Chest: {value*3456:,}\nChunk: {value*98304:,}")
+        rand_selection.set(f"Block: {ran_block}\nValue: {value}\nStack: {value*64:,.0f}\nChest: {value*1728:,.0f}\nDouble Chest: {value*3456:,.0f}\nChunk: {value*98304:,.0f}")
 
 def on_select(event):
     print(f"Selected: {block_box.get()}")
+    current_blocks = getattr(bl, block_catagorys[current_color_index-1])
+    print(f"Value: {current_blocks[block_box.get()]}")
     value = current_blocks[block_box.get()]
     if use_diamonds.get() == True:
-        value = round(value/1500, 2)
-        rand_selection.set(f"Block: {block_box.get()}\nValue: {value}\nStack: {value*64:,}\nChest: {value*1728:,}\nDouble Chest: {value*3456:,}\nChunk: {value*98304:,}")
+        value = value/1500
+        value = round(value, 2)
+        rand_selection.set(f"Block: {block_box.get()}\nValue: {value}\nStack: {value*64:,.0f}\nChest: {value*1728:,.0f}\nDouble Chest: {value*3456:,.0f}\nChunk: {value*98304:,.0f}")
     else:
-        rand_selection.set(f"Block: {block_box.get()}\nValue: {value}\nStack: {value*64:,}\nChest: {value*1728:,}\nDouble Chest: {value*3456:,}\nChunk: {value*98304:,}")    
+        rand_selection.set(f"Block: {block_box.get()}\nValue: {value}\nStack: {value*64:,.0f}\nChest: {value*1728:,.0f}\nDouble Chest: {value*3456:,.0f}\nChunk: {value*98304:,.0f}")    
 #------------------------------------------------ input frame ------------------------------------------------
 
 #define label and button for block chatagory switching and redo button
@@ -91,14 +97,15 @@ button_switch = ttk.Button(master=input_frame, textvariable=button_color, text="
 diamond_button = ttk.Checkbutton(master=input_frame, text="Use Diamonds Instead", variable=use_diamonds, command=diamond_switch)
 
 # Button to select a new random block
-redo_button = ttk.Button(master=input_frame, text="Block", command=random_block)
+redo_button = ttk.Button(master=input_frame, text="Random Block", command=random_block)
 
 # Label to display the current block selection
 current_block_selection = ttk.Label(master=window, textvariable=rand_selection, font="system 25 bold")
 
 
 # Combobox for selecting a specific block within the selected category
-block_box = ttk.Combobox(master=input_frame, values=list(current_blocks.keys()))
+block_box_list = list(current_blocks.keys())
+block_box = ttk.Combobox(master=input_frame, values=block_box_list, state="readonly")
 block_box.set('Select a block')
 block_box.bind("<<ComboboxSelected>>", on_select)
 
